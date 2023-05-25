9c6d832d29520dd86a6d8ed506d6db663bed0a4f
InDate <joshua@shanksy.me>
2023-05-25 10:14:44 +0800

    wip: Added file and error message parsing to debug command.

--- r2/command_classes/debug_command.py
+++ r2/command_classes/debug_command.py
@@ -8,28 +8,38 @@ class DebugCommand(BaseCommand):
     def __init__(self, io, coder):
         super().__init__(io, coder)
 
+    def parse_input(self, args, files):
+        if args.isspace() or args == '':
+            self.io.tool_error("Provide a file name to use this command")
+            return None
+        elif args.split() != 1:
+            self.io.tool_error("Provide an error message to use this command")
+            return None
+
+        file_path, error_message = args.split()
+
+        if not [file for file in files if file_path in file]:
+            file_path = self.create_files(file_path)
+
+        return file_path, error_message
+
     def run(self, args, **kwargs):
         ''' 
             running debug command means adding files to the chat for the robot to debug with.
             running debug means passing debug messages in argument
         '''
+        files = self.coder.get_all_relative_files()
+
+        if (isinstance(args, str)):
+            args, error_message = self.parse_input(args, files)
 
         if kwargs.get("debug"):
             debug_message = kwargs.get("error_message")
             self.get_help(args, debug_message)
-        elif args.isspace():
-            self.io.tool_error("Add a file name to use this command")
-        else:
-            matched_files = []
-            files = self.coder.get_all_relative_files()
-
-            for word in args.split():
-                matched_files = [file for file in files if word in file]
-                break
-
-            for matched_file in matched_files:
-                self.io.queue.enqueue(
-                    ('execute_command', '/add', matched_file), to_front=True)
+        elif args and error_message:
+            self.io.queue.enqueue(('execute_command', '/add', args))
+            self.io.queue.enqueue(('send_new_command_message', error_message,
+                                   "Expert Debugger"))
 
     def get_help(self, failing_file, error_message):
         # TODO; Pass exceptions from unit tests that do not contain formatting.
@@ -38,11 +48,13 @@ class DebugCommand(BaseCommand):
 
         messages = [
             self.coder.get_message("system", prompts.system_reminder),
-            self.coder.get_message("user", prompts.debug.format(message=cleaned_up_message))
+            self.coder.get_message(
+                "user", prompts.debug.format(message=cleaned_up_message))
         ]
         self.io.queue.enqueue(('send_new_command_message', messages,
                               "Expert Debugger"), to_front=True)
-        self.io.queue.enqueue(('execute_command', '/add', failing_file), to_front=True)
+        self.io.queue.enqueue(
+            ('execute_command', '/add', failing_file), to_front=True)
 
     def completions_debug(self, partial):
         files = set(self.coder.get_all_relative_files())
