11b1dbd8d7664ff72adcd838818e1ac29983d76d
InDate <joshua@shanksy.me>
2023-05-25 16:08:41 +0800

    r2: Updated extract_function_definitions to capture entire function definitions as strings.
    # Context:
    USER:
    USER: I want extract_function_definitions to find methods by their name and then extract the entire def storing it as a string
    ASSISTANT: To achieve this, we will modify the `extract_function_definitions` function in the `r2/coder_classes/code_parser.py` file. We will update the regular expression pattern to capture the entire function definition and store it as a string.
    
    Here's the step-by-step plan:
    
    1. Update the regular expression pattern to capture the entire function definition.
    2. Modify the loop to append the entire matched function definition to the `function_definitions` list.
    
    Here's the code change:
    
    ```python
    r2/coder_classes/code_parser.py
    <<<<<<< ORIGINAL
    import re
    
    def extract_function_definitions(code):
        """Extracts function definitions from the given Python code."""
        function_definitions = []
        pattern = re.compile(r"def\s+(\w+)\s*\(")
        matches = pattern.finditer(code)
        for match in matches:
            function_definitions.append(match.group(1))
        return function_definitions
    =======
    import re
    
    def extract_function_definitions(code):
        """Extracts function definitions from the given Python code."""
        function_definitions = []
        pattern = re.compile(r"(def\s+\w+\s*\(.*?\):(?:\n(?:\s*#.*\n)*\s*(?:[^#\n].*?(?:\n|$))+)+)", re.DOTALL)
        matches = pattern.finditer(code)
        for match in matches:
            function_definitions.append(match.group(1))
        return function_definitions
    >>>>>>> UPDATED
    ```
    
    This change will make the `extract_function_definitions` function capture the entire function definition, including its body, and store it as a string in the `function_definitions` list.

--- r2/coder_classes/code_parser.py
+++ r2/coder_classes/code_parser.py
@@ -3,7 +3,7 @@ import re
 def extract_function_definitions(code):
     """Extracts function definitions from the given Python code."""
     function_definitions = []
-    pattern = re.compile(r"def\s+(\w+)\s*\(")
+    pattern = re.compile(r"(def\s+\w+\s*\(.*?\):(?:\n(?:\s*#.*\n)*\s*(?:[^#\n].*?(?:\n|$))+)+)", re.DOTALL)
     matches = pattern.finditer(code)
     for match in matches:
         function_definitions.append(match.group(1))
